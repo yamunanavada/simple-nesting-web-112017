82fd83263742970f6b3d5c6c46b82705b2c50069
Yamuna Navada <yamunanavada@gmail.com>
2017-11-04 15:30:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -84,6 +84,7 @@ def adding_matz
         }
      }
 		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+		 programmer_hash
 
 end
 
